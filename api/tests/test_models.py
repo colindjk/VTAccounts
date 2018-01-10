b0da5bd9a2278a0fc6ae862ba101fa51e9b8dcc9
Colin Kirkpatrick <kibbles@kibshost.kibsdomain>
2018-01-10 17:37:34 -0500

    Added tests, current tests are empty, will be used to test indirect / fringe

---
+++ api/tests/test_models.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+
+from api import models
+
+class TestTransaction(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+
+        pass
+
+    def setUp(self):
+
+        pass
+
+    def test_transaction_basic(self):
+        pass
+
+    def test_transaction_create_indirect(self):
+        pass
+
+    def test_transaction_create_fringe(self):
+        pass
+
+    def test_transaction_update_indirect(self):
+        pass
+
+    def test_transaction_update_fringe(self):
+        pass
+
