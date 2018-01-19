3b54b3454fb7d76a6f1e0fb6e17a5a74f28c8300
Colin Kirkpatrick <kibbles@kibshost.kibsdomain>
2018-01-19 13:58:54 -0500

    Added support for bootstrap3 import

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,15 @@ module.exports = {
         loader: 'babel-loader', 
         query: { presets: ['react'] },
       },
+      {
+        test: /\.css$/,  
+        include: /node_modules/,  
+        loaders: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }
     ],
   },
 
