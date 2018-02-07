6c8ca2a107ba0a3e0973315c94bc037b0e522558
Colin Kirkpatrick <kibbles@kibshost.kibsdomain>
2018-02-06 21:04:45 -0500

    Created modules for Summary views

---
+++ frontend/src/containers/grid/GridWrapper.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const GridWrapper = () => (
+  <div>
+    <h1>Dashboard</h1>
+  </div>
+);
+
+export default GridWrapper;
+
---
+++ frontend/src/pages/Home/EmployeeSummary.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EmployeeSummary = () => (
+  <div>
+    <h1>Dashboard</h1>
+  </div>
+);
+
+export default EmployeeSummary;
