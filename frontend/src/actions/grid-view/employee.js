d2509c45f4784e8e7503f110ce6a6f7db5a86fba
Colin Kirkpatrick <kibbles@kibbles.laptop>
2018-03-15 10:45:46 -0400

    Reimplemented employee salaries populate function to not use javascript date module

--- frontend/src/actions/grid-view/employee.js
+++ frontend/src/actions/grid-view/employee.js
@@ -22,6 +22,54 @@ export const populateSalaries = (employees, range, accounts) => {
   return newEmployees
 }
 
+// TODO: Reimplement using the actual month / date / year values.
+
+// Three cases:
+// dateA > dateB => 1
+// dateA < dateB => -1
+// dateA===dateB => 0
+const compareDates = (dateA, dateB) => {
+  if (dateA === dateB) return 0
+  let valsA = dateA.split('-').map(str => parseInt(str))
+  let valsB = dateB.split('-').map(str => parseInt(str))
+
+  // year  => 0
+  // month => 1
+  // day   => 2
+  if (valsA[0] > valsB[0] ||
+      valsA[0] === valsB[0] && valsA[1] > valsB[1] ||
+      valsA[0] === valsB[0] && valsA[1] === valsB[1] && valsA[2] > valsB[2])
+  {
+    return 1
+  } else {
+    return -1
+  }
+}
+
+// Gets the latest salary that is associated with the given date. Return `index`
+const getLatestSalary = (salaries, date) => {
+  let salaryIndex = 0
+
+  // The classic `while switch`
+  while (true) switch(compareDates(date, salaries[salaryIndex])) {
+    case 1:
+      salaryIndex++
+      break;
+    case -1:
+      if (salaryIndex !== 0) {
+        return salaryIndex - 1
+      } else {
+        return salaryIndex // aka return 0
+      }
+    case 0:
+      return salaryIndex
+    default:
+      console.error("ERORR::getLatestSalary => Fall through case in compareDates")
+  }
+
+
+}
+
 // Modifies the given account to populate it with the given data.
 // Unfortunately matching dates in javascript is a bit janky, so we use the
 // date strings when finding an exact date match, and Date objects otherwise.
@@ -29,32 +77,34 @@ export const getEmployeeSalaries = (employee, range) => {
   const salariesArray = employee.salaries
   if (salariesArray === undefined) { console.log("ERROR: No salaries found") }
 
-  var virtualSalary = { total_ppay: 0, virtual: true, employee: employee.id }
-  var salaries = {}, salaryIndex = 0
+  var virtualSalary = { total_ppay: 0, isVirtual: true, employee: employee.id }
+  var salaries = {}
+  var salaryIndex = getLatestSalary(salaries, range[0].date)
+
   range.forEach(date => {
     // Overflow / empty salaries
     if (salariesArray.length === salaryIndex) {
       salaries[date] = { ...virtualSalary, date }
-      return
+      return // forEach's `continue`
     }
 
-    const salary = salariesArray[salaryIndex]
-    let curDate = Date.parse(date)
-    let salaryDate = Date.parse(salary.date)
-
-    if (curDate < salaryDate) {
-      salaries[date] = { ...virtualSalary, date }
-    }
-    else if (date === salary.date) {
-      salaries[date] = { ...salary }
-      let { total_ppay } = salary
-      virtualSalary = { ...virtualSalary, total_ppay }
-      salaryIndex++
-    }
-    else if (curDate > salaryDate) {
-      // TODO: fix this so that it iterates the salaries until match / less than
-      let { total_ppay } = salary
-      salaries[date] = { ...virtualSalary, total_ppay, date: salary.date }
+    // Copy over the salary, this will then be virtualized or directly applied
+    // to salaries dict.
+    let salary = { ...salariesArray[salaryIndex] }
+    switch (compareDates(date, salary.date)) {
+      case 1:
+        // This should only every happen once, TODO: Make a better case here.
+        salary = { ...salary, isVirtual: true }
+      case 0:
+        salaries[date] = salary
+        virtualSalary = { ...salary, isVirtual: true }
+        salaryIndex++
+        return
+      case -1:
+        salaries[date] = { ...virtualSalary } // not there yet
+        return
+      default:
+        console.error("ERORR::getEmployeeSalaries => Fall through case in compareDates")
     }
   })
 
