e1de8b20e928d3263f174d3c3af60432cec4876d
Colin Kirkpatrick <kibbles@kibbles.laptop>
2018-06-24 14:31:35 -0400

    Created for shared functions and export structure

---
+++ frontend/src/selectors/payments/index.js
@@ -0,0 +1,14 @@
+
+export const defaultAggregates = { paid: 0, budget: 0, count: 0, }
+
+// Helper function which aggregates two payments together.
+export const combinePayments = (paymentA, paymentB) => {
+  return { 
+    ...paymentA,
+    paid: paymentA.paid + paymentB.paid,
+    budget: paymentA.budget + paymentB.budget,
+    count: paymentA.count || 0 + paymentB.count || 0,
+  }
+}
+
+
