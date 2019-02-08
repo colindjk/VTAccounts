d724f2ce4fa2040617f4df68035e9e02b9600eb2
Colin Kirkpatrick <kibbles@kibbles.laptop>
2019-02-08 13:39:05 -0500

    Updated populate script to properly initialize the database

--- backend/populate.sh
+++ backend/populate.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # [ ] - Create script to execute "populate_*" functions:
-#     1. populate_pay_periods
-#     2. populate_accounts
-#     3. populate_fringes & populate_indirect
+#     1. populate_accounts
+#     2. populate_fringes & populate_indirect
+source ./../.env/bin/activate
+
+./manage.py populate_accounts
+./manage.py populate_fringes
+./manage.py populate_indirects
+
